43bdbacc0749220b705efbb0919d0a1295d4764b
Ethan Fahnestock <efahnest@u.rochester.edu>
2020-12-08 00:21:13 -0500

    adding plots

---
+++ plot.py
@@ -0,0 +1,78 @@
+import pySALEPlot as psp
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Need this for the colorbars we will make on the mirrored plot
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+# If viridis colormap is available, use it here
+try:
+    plt.set_cmap('viridis')
+except:
+    plt.set_cmap('YlGnBu_r')
+
+# This example plotting script designed to plot 
+# damage and pressure for the demo2D example
+
+# Make an output directory
+dirname = './generatedPlots/2kmPlots'
+
+# Open the datafile
+model = psp.opendatfile('./data/job-1_2km/jdata.dat')
+
+# Set the distance units to km
+model.setScale('km')
+
+# Set up a pylab figure
+fig = plt.figure(figsize=(8, 4))
+
+ax = fig.add_subplot(111, aspect='equal')
+divider = make_axes_locatable(ax)
+
+# Loop over timesteps
+for i in np.arange(0,5000):
+
+    # Set the axis labels
+    ax.set_xlabel('r [km]')
+    ax.set_ylabel('z [km]')
+
+    # Set the axis limits
+    ax.set_xlim([-50, 50])
+    ax.set_ylim([-50, 50])
+
+    # Read the time step 'i' from the datafile: 
+    # read two or more fields by making a list of their abbreviations
+    step=model.readStep(['Pre', 'Dam'],i)
+    
+    # -- the classic iSALEPlot mirrored setup. Plot the second field
+    # -- using negative x values
+    p1=ax.pcolormesh(model.x, model.y, step.data[1],
+            cmap='Blues_r', vmin=0., vmax=1.)
+    p2=ax.pcolormesh(-model.x, model.y, step.data[0],
+            vmin=0, vmax=1.0E9)
+
+    # Material boundaries
+    ax.contour(model.xc, model.yc, step.cmc[0], levels=[0.5], colors='k', linewidths=1.)
+    ax.contour(-model.xc, model.yc, step.cmc[0], levels=[0.5], colors='k', linewidths=1.)
+
+    # Add colorbars; only need to do this once
+    if i == 0: 
+        # Right plot legend
+        cx = divider.append_axes("right", size="5%", pad=0.7)
+        cbr = fig.colorbar(p1, cax=cx)
+        cbr.set_label(psp.longFieldName(step.plottype[1]))
+        # Left plot legend
+        cx = divider.append_axes("left", size="5%", pad=0.7)
+        cbl = fig.colorbar(p2, cax=cx)
+        cbl.set_label(psp.longFieldName(step.plottype[0]))
+        # Need to set the labels on the left for this colorbar
+        cx.yaxis.tick_left()
+        cx.yaxis.set_label_position('left')
+
+    ax.set_title('demo2D: T = {: 5.2f} s'.format(step.time))
+    
+    # Save the figure
+    fig.savefig('{}/DamPre-{:05d}.png'.format(dirname, i), format='png', dpi=300)
+    
+    # Remove the field, ready for the next timestep to be plotted
+    ax.cla()
--- plot_tracer.py
+++ plot_tracer.py
@@ -2,16 +2,21 @@ import pySALEPlot as psp
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # This example plotting script designed to plot 
 # a vertical pressure profile of the Chicxulub example
 
 # Open the datafile
-model=psp.opendatfile('../data/jdata.dat')
+model=psp.opendatfile('/media/sf_Public/data/job-1_6km/jdata.dat')
+#model=psp.opendatfile('./data/job-1_4km/jdata.dat')
 
 # Set the distance units to mm
 model.setScale('km')
 
 [model.tru[u].truInfo() for u in range(model.tracer_numu)]
+
+step0=model.readStep('TrM',0)
+stepTrT0 = model.readStep('TrT', 0)
 
 # Set up a pyplot figure
 fig = plt.figure(figsize=(20, 10))
@@ -23,35 +28,26 @@ ax.set_xlabel('r [km]')
 ax.set_ylabel('z [km]')
 
 # Set the axis limits
-ax.set_xlim(-10,10)
-ax.set_ylim(-10,5)
-
-sp_idx = 0
-for i in range(1000):
-    stp = model.readStep('TrM',i)
-    if stp.time > 0.1:
-        sp_idx = i
-        break
-
-print("Chose step {}".format(sp_idx))
+rad_km = 50
+depth_km = 30
+height_km = 100
+ax.set_xlim(-rad_km,rad_km)
+ax.set_ylim(-depth_km,height_km)
+#stp = model.readStep('TrM',i)
 
 # Read the time steps from the datafile
 nstep=0
-step0=model.readStep('TrM',0)
-stepn=model.readStep('TrM',sp_idx)
-stepTrT0 = model.readStep('TrT', 0)
-stepTrT = model.readStep('TrT',sp_idx)
+stepn=model.readStep('TrM',0)
+stepTrT = model.readStep('TrT',0)
 # Plot the pressure field
 for u in range(model.tracer_numu): #loop over tracer clouds
     tstart = model.tru[u].start
     tend = model.tru[u].end
-    scat = ax.scatter(step0.xmark[tstart:tend],step0.ymark[tstart:tend],
+    scat = ax.scatter(stepn.xmark[tstart:tend],stepn.ymark[tstart:tend],
                       c=stepn.TrM[tstart:tend],s=4,vmin=0,vmax=5)
 
-    scat2 = ax.scatter(-stepTrT.xmark[tstart:tend],stepTrT.ymark[tstart:tend],
-            c=stepTrT.TrT[tstart:tend],s=4,vmin=0,vmax=3000)
-
-
+    scat2 = ax.scatter(-stepn.xmark[tstart:tend],stepn.ymark[tstart:tend],
+                      c=stepn.TrM[tstart:tend],s=4,vmin=0,vmax=5)
 
 
 cb=fig.colorbar(scat)
@@ -59,6 +55,31 @@ cb.set_label('Material Properties')
 cb2=fig.colorbar(scat2)
 cb2.set_label('Peak Temperature (K)')
 ax.set_title('Ice: T = {: 5.2f} s'.format(stepTrT.time))
+for i in np.arange(0,5000,10):
+    # Set up a pyplot figure
+    ax.cla()
+    rad_km = 50
+    depth_km = 30
+    height_km = 100
+    ax.set_xlim(-rad_km,rad_km)
+    ax.set_ylim(-depth_km,height_km)
+    
+    nstep=0
+    stepn=model.readStep('TrM',i)
+    #stepTrT = model.readStep('TrT',i)
+    # Plot the pressure field
+    for u in range(model.tracer_numu): #loop over tracer clouds
+        tstart = model.tru[u].start
+        tend = model.tru[u].end
+        scat = ax.scatter(step0.xmark[tstart:tend],step0.ymark[tstart:tend],
+                          c=step0.TrM[tstart:tend],s=4,vmin=0,vmax=5)
 
-# Save the figure
-fig.savefig('./Plots/Figure4.png')
+        scat2 = ax.scatter(-stepn.xmark[tstart:tend],stepn.ymark[tstart:tend],
+                          c=stepn.TrM[tstart:tend],s=4,vmin=0,vmax=5)
+
+        #scat2 = ax.scatter(-stepTrT.xmark[tstart:tend],stepTrT.ymark[tstart:tend],
+        #        c=stepTrT.TrT[tstart:tend],s=4,vmin=0,vmax=3000)
+
+    ax.set_title('Ice: T = {: 5.2f} s'.format(stepTrT.time))
+    # Save the figure
+    fig.savefig('./generatedPlots/Figure{:05d}.png'.format(i))
